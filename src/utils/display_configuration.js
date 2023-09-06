7d8e0bea8cce0d6c4240f26b5380369d3d6eae1a
RibhuGuha <ribhu@replicacia.com>
2023-09-06 12:58:36 +0530

    initial commit

---
+++ src/utils/display_configuration.js
@@ -0,0 +1,68 @@
+export const tripViewConfig = {
+Details: [
+    {
+      key: "EndTime",
+      value: "EndTime",
+      type: "date",
+      editable: false,
+      required: true
+    },
+    {
+      key: "StartTime",
+      value: "StartTime",
+      type: "date",
+      editable: false,
+      required: true
+    },
+    {
+      key: "TripId",
+      value: "TripId",
+      type: "text",
+      editable: false,
+      required: true
+    },
+  ],
+Type: [
+    {
+      key: "TripRoute",
+      value: "Route Name",
+      type: "lookup",
+      editable: true,
+      required: true
+    },
+  ],
+}
+export const routeViewConfig = {
+Details: [
+    {
+      key: "From",
+      value: "From",
+      type: "text",
+      editable: false,
+      required: true
+    },
+    {
+      key: "RouteName",
+      value: "RouteName",
+      type: "text",
+      editable: false,
+      required: true
+    },
+    {
+      key: "RouteId",
+      value: "RouteId",
+      type: "text",
+      editable: false,
+      required: true
+    },
+    {
+      key: "To",
+      value: "To",
+      type: "text",
+      editable: false,
+      required: true
+    },
+  ],
+Type: [
+  ],
+}
